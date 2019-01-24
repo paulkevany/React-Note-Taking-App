4c776ffbb60bf293278ab0fee1a50c369ebc1f57
Paul Kevany <pkevany25@gmail.com>
2019-01-24 08:28:11 +0000

    Update config.js

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -3,23 +3,23 @@ export default{
 	MAX_ATTACHMENT_SIZE: 5000000,
 
 	s3: {
-		REGION: "eu-west-1",
-		BUCKET: "notepad-app-upload"
+		REGION: 
+		BUCKET: 
 
 	},
 
 	apiGateway: {
 
-		REGION: "eu-west-1",
-		URL: "https://yoxbpyrbcb.execute-api.eu-west-1.amazonaws.com/prod"
+		REGION: 
+		URL: 
 	},
 
 	cognito: {
 
-		REGION: "eu-west-1",
-		USER_POOL_ID: "eu-west-1_UdsLQC4tB",
-		APP_CLIENT_ID: "39b988iutnkut80en0gmp47m17",
-		IDENTITY_POOL_ID: "eu-west-1:618d940e-352d-4dbb-945a-16a399643491"
+		REGION:
+		USER_POOL_ID: 
+		APP_CLIENT_ID: 
+		IDENTITY_POOL_ID: 
 
 
 	}
